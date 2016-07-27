037613f90b829f267bd82e21ee11491fbf456c56
Jacob Neeley <blitzchamp13@gmail.com>
2016-07-26 22:15:30 -0500

    Added employee inserts

--- hospital_db.sql
+++ hospital_db.sql
@@ -122,3 +122,37 @@ CREATE TABLE diagnosesGiven (
 CREATE TABLE services (
 
 );
+
+
+insert into employees values
+	(000000001, 'CONNIE ARDATH', '1982-2-6', TRUE, 'doctor'),
+	(000000002, 'KENNITH KATHRYNE', '1985-1-1', TRUE, 'doctor'),
+	(000000003, 'WARD ROWENA', '1983-5-28', TRUE, 'doctor'),
+	(000000004, 'CLAUDIO HILDA', '1987-3-26', TRUE, 'doctor'),
+	(000000005, 'ELLIS ARLETTE', '1981-2-12', FALSE, 'nurse'),
+	(000000006, 'CHRISTOPHER YVONE', '1982-1-26', FALSE, 'nurse'),
+	(000000007, 'SOLOMON SHERLEY', '1983-10-27', FALSE, 'nurse'),
+	(000000008, 'MODESTO MICHAELE', '1985-8-2', FALSE, 'nurse'),
+	(000000009, 'JEFFRY MI', '1984-3-1', FALSE, 'nurse'),
+	(000000010, 'MOSES ROMONA', '1984-5-3', FALSE, 'technicians'),
+	(000000011, 'MOHAMED ELAYNE', '1987-9-24', FALSE, 'technicians'),
+	(000000012, 'THEODORE NOBLE', '1985-8-10', FALSE, 'technicians'),
+	(000000013, 'LAMAR PHILOMENA', '1989-10-12', FALSE, 'technicians'),
+	(000000014, 'BRANT VERONA', '1984-5-7', FALSE, 'technicians'),
+	(000000015, 'PORTER CARLOS', '1986-9-16', FALSE, 'staff'),
+	(000000016, 'BLAIR BERENICE', '1989-3-1', FALSE, 'staff'),
+	(000000017, 'OTIS JADE', '1986-8-26', FALSE, 'staff'),
+	(000000018, 'LAVERNE BEVERLY', '1984-6-24', FALSE, 'staff'),
+	(000000019, 'MERLIN QUEEN', '1987-7-14', FALSE, 'staff'),
+	(000000020, 'CHAUNCEY INGA', '1983-10-19', FALSE, 'administrator'),
+	(000000021, 'SIMON WALTER', '1989-11-18', FALSE, 'administrator'),
+	(000000022, 'QUINCY KIMBRA', '1980-9-12', FALSE, 'administrator'),
+	(000000023, 'MASON EARNESTINE', '1989-5-8', FALSE, 'administrator'),
+	(000000024, 'RICKIE PHILLIP', '1981-2-25', FALSE, 'administrator'),
+	(000000025, 'ELOY ANDREE', '1988-4-13', FALSE, 'volunteer'),
+	(000000026, 'JARROD BUFORD', '1984-7-2', FALSE, 'volunteer'),
+	(000000027, 'TONEY HUGO', '1989-9-4', FALSE, 'volunteer'),
+	(000000028, 'GILBERTO VIVIAN', '1985-6-5', FALSE, 'volunteer'),
+	(000000029, 'QUENTIN IESHA', '1981-12-7', FALSE, 'volunteer'),
+	(000000030, 'TEDDY NEREIDA', '1989-10-27', FALSE, 'volunteer');
+
