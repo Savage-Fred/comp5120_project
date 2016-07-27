4c0d29b1db81c15341970596ed64d5a0fed551b3
Will McCarty <wpm1414@gmail.com>
2016-07-26 21:01:19 -0500

    done, but needs to be reviewed

--- hospital_db.sql
+++ hospital_db.sql
@@ -87,38 +87,71 @@ CREATE TABLE admissions (
 );
 
 /*
-   - assignedDoctors(pid, doctor)*/
+   - assignedDoctors(pid*, doctor*)
+   - Doctors are assigned to patients. 
+   - Multiple doctors can be assigned to a single patient*/
 CREATE TABLE assignedDoctors (
-
+   pid         varchar(10),
+   doctor      varchar(10),
+   PRIMARY KEY (pid, doctor)
+   FOREIGN KEY (pid)    REFERENCES patients  (pid)
+   FOREIGN KEY (doctor) REFERENCES employees (eid)
 );
 
 /*
-   - treatments(tid, name)*/
+   - treatments(trid*, name)*/
 CREATE TABLE treatments (
-
+   trid        varchar(10),
+   tname       text,
+   PRIMARY KEY (trid)
 );
 
 /*
-   - diagnoses(did, name)*/
-
+   - diagnoses(did*, name)*/
 CREATE TABLE diagnoses (
-
+   did         varchar(10),
+   dname       text,
+   PRIMARY Key (did)
 );
 
 /*
-   - treatmentsGiven(pid, doctor, tid, eid, timestamp)*/
+   - treatmentsGiven(pid*, doctor, tid, eid, timestamp*)
+   - treatments are ordered by a doctor and administered by an appropriate employee */
 CREATE TABLE treatmentsGiven (
-
+   pid         varchar(10),
+   doctor      varchar(10),
+   trid        varchar(10),
+   eid         varchar(10),
+   tTime       timestamp,
+   FOREIGN KEY (pid)    REFERENCES patients   (pid)
+   FOREIGN KEY (doctor) REFERENCES employees  (eid)
+   FOREIGN KEY (trid)   REFERENCES treatments (trid)
+   FOREIGN KEY (eid)    REFERENCES employees  (eid)
+   PRIMARY KEY (pid, tTime)
 );
 
 /*
-   - diagnosesGiven(pid, doctor, did, timestamp)*/
+   - diagnosesGiven(pid*, doctor, did, timestamp*)*/
 CREATE TABLE diagnosesGiven (
-
+   pid         varchar(10),
+   doctor      varchar(10),
+   did         varchar(10),
+   dTime       timestamp,
+   FOREIGN KEY (pid)    REFERENCES patients  (pid)
+   FOREIGN KEY (doctor) REFERENCES employees (eid)
+   FOREIGN KEY (did)    REFERENCES diagnoses (did)
+   PRIMARY KEY (pid, dTime)
 );
 
 /*
-   - services(vid, day, location) */
+   - services(vid*, day*, location) 
+   - volunteers provide services for a few days each week 
+   - they work in different locations*/
 CREATE TABLE services (
-
+   vid       varchar(10),
+   sday      date,
+   slocation text NOT NULL
+      CHECK (slocation IN ('gift shop', 'information desk', 'snack cart', 'reading cart')),
+   FOREIGN KEY (vid) REFERENCES volunteers (vid)
+   PRIMARY KEY (vid, sday)
 );
