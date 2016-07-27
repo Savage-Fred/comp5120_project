a5627a9f3f4a30b4d97642609025c111997fe37f
Will McCarty <wpm1414@gmail.com>
2016-07-27 17:41:20 -0500

    Added PatientInfo.java class structure.

---
+++ PatientInfo.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+
+public class PatientInfo {
+
+   ResultSet B1() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B2() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B3() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B4() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B5() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B6() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B7() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B8() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B9() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet B10() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt.main.connection.createStatement();
+      return stmt.executeQuery(query);     
+   }
+}
