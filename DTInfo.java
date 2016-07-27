d604d61bbcdb2e11bf19de20568c01ceb92b18bb
Will McCarty <wpm1414@gmail.com>
2016-07-27 17:47:59 -0500

    PatientInfo and DTInfo added

---
+++ DTInfo.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class DTInfo {
+   ResultSet C1() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C2() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C3() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C4() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C5() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C6() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C7() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+   ResultSet C8() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.execututeQuery(query);
+   }
+}
---
+++ EmployeeInfo.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+public class EmployeeInfo {
+   ResultSet D1() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet D2() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet D3() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet D4() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet D5() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet D6() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+   ResultSet D7() throws SQLException, ClassNotFoundException {
+      String query = "";
+      Statement stmt = main.connection.createStatement();
+      return stmt.executeQuery(query);
+   }
+}
